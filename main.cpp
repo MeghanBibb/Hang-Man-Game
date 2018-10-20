e6b5a4474c29ec7ea24483372e4e2338cee18994
Meghan Bibb <33154839+SicEmBears21@users.noreply.github.com>
2018-10-20 09:46:30 -0500

    Completion of user guessing portion of the game
    The user can now guess a randomly generated word.

--- main.cpp
+++ main.cpp
@@ -1,113 +1,7 @@
-
 // Meghan Bibb
-// 08/01/2018
-// This file utilizes the MyVector object to allow a user
+// 10/20/2018
+// This file utilizes a hashTable to allow a user
 //      to play as the guesser or asked individual in a game of hang man
-//      The outline/ flowchart of this program is located
-//           at the bottom of this file
-
-#include <iostream>
-#include <fstream>
-#include <unordered_map>
-#include <vector>
-#include "MyVector.h"
-
-using namespace std;
-
-int hashKey(string word, int numItems);
-
-int main() {
-    int numGuesses = 4;
-    ifstream input;
-    string word;
-    int key, numItems = 0, randIndex = 0;
-    unordered_map<int,string> dictionary;
-    pair<int,string> myPair;
-
-    input.open("alphabet.txt");
-    if(!input.is_open()){
-        cout << "file not open" << endl;
-        return 0;
-    }
-
-    //fills map
-    do{
-        input >> word;
-        numItems++;
-        key = hashKey(word, numItems);
-        myPair.first = key;
-        myPair.second = word;
-        dictionary.insert(myPair);
-    }while(!input.eof());
-
-    randIndex = rand() % numItems;
-    word = dictionary.at(randIndex);
-
-    char letter;
-    bool valid = false, reveal = false;
-    do{
-        cout << "If you would like to guess a word input 'g': " << endl;
-        cout << "If you would like to have your word guessed insert 'w' : " << endl;
-        cin >> letter;
-        if(letter == 'G' || letter == 'W'){
-            //32 is the difference in ASCII values between upper and lower case
-            letter += 32;
-        }
-        if(letter == 'g' || letter == 'w'){
-            valid = true;
-        }
-    }while(!valid);
-
-    bool *foundNdx = new bool[word.size()];
-    bool found = false;
-    int wordSize = word.size();
-    for(int i = 0; i < wordSize; i++){
-        foundNdx[i] = false;
-    }
-
-    if(letter == 'g')
-    do{
-        cout << "Enter a letter" << endl;
-        cin >> letter;
-        for(unsigned int i = 0; i < wordSize; i++){
-            if(word.at(i) == letter){
-                reveal = true;
-                foundNdx[i] = true;
-            }
-        }
-        if(!reveal){
-            numGuesses--;
-        }else {
-            found = true;
-            for(int i = 0; i < wordSize && found; i++){
-                if(word.at(i) != true){
-                    found = false;
-                }
-            }
-        }
-    }while(numGuesses > 0 || !found);
-
-    /*
-    (*it).first;             // the key value (of type Key)
-    (*it).second;            // the mapped value (of type T)
-    (*it);                   // the "element value" (of type pair<const Key,T>)
-    ifstream input;
-    */
-
-
-
-    return 0;
-}
-
-int hashKey(string word, int numItems){
-    int num = 0;
-    for(int i = 0; i < word.size(); i++){
-        num += word.at(i)*3;
-    }
-    num = num % numItems;
-    return num;
-}
-
 
 /*
  *  Draft of expectations / flow of the hangman program
@@ -135,3 +29,155 @@ int hashKey(string word, int numItems){
  *               - if guesses > 0, repeat the loop
  *
  */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+#include <unordered_map>
+#include <cctype>
+
+const int LETTERS_IN_ALPHABET = 26;
+
+using namespace std;
+
+int hashKey(string word, int numItems);
+void display(int numGuesses, string word, bool letters[],bool isIn[], ostream &os = cout);
+
+int main() {
+    int numGuesses = 5;
+    ifstream input;
+    string word;
+    int key, numItems = 0, randIndex = 0, wordSize = 0;
+    unordered_map<int,string> dictionary;
+    pair<int,string> myPair;
+    bool guessedLetters[LETTERS_IN_ALPHABET];
+    bool found = false;
+
+    //input file
+    input.open("alphabet.txt");
+    if(!input.is_open()){
+        cout << "file not open" << endl;
+        return 0;
+    }
+
+    //fills map
+    do{
+        input >> word;
+        numItems++;
+        key = hashKey(word, numItems);
+        myPair.first = key;
+        myPair.second = word;
+        dictionary.insert(myPair);
+    }while(!input.eof());
+
+    char letter;
+    bool valid = false, reveal = false;
+    do{
+        cout << "If you would like to guess a word input 'g': " << endl;
+        cout << "If you would like to have your word guessed insert 'w' : " << endl;
+        cin >> letter;
+        if(letter == 'G' || letter == 'W'){
+            //32 is the difference in ASCII values between upper and lower case
+            letter += 32;
+        }
+        if(letter == 'g' || letter == 'w'){
+            valid = true;
+        }
+    }while(!valid);
+
+    randIndex = rand() % numItems;
+    word = dictionary.at(randIndex);
+    wordSize = word.size();   //keep this, its efficient in limiting function calls
+    bool *foundNdx = new bool[wordSize];
+
+    //makes all letters lowercase
+    for(short i = 0; i < wordSize; i++){
+        word[i] = tolower(word[i]);
+    }
+
+    //default state of guessedLetters is false
+    for(int i = 0; i < LETTERS_IN_ALPHABET; i++){
+        guessedLetters[i] = false;
+    }
+
+    //default state of foundNdx is false
+    for(int i = 0; i < wordSize; i++){
+        foundNdx[i] = false;
+    }
+
+    //user guesses a word
+    if(letter == 'g'){
+        do{
+            cout << "Enter a letter" << endl;
+            cin >> letter;
+            letter = tolower(letter);
+
+            //determines whether letter is in the word
+            for(unsigned int i = 0; i < wordSize; i++){
+                if(word.at(i) == letter){
+                    reveal = true;
+                    foundNdx[i] = true;
+                }
+            }
+            guessedLetters[letter-'a'] = true;
+
+            //if letter is in the word, determine if word has been guessed
+            //if letter is not in the word, decrement guesses
+            if(!reveal){
+                numGuesses--;
+            }else {
+                found = true;
+                for(int i = 0; i < wordSize && found; i++){
+                    if(!foundNdx[i]){
+                        found = false;
+                    }
+                }
+                reveal = false;
+            }
+
+            display(numGuesses, word, guessedLetters, foundNdx);
+
+        }while(numGuesses > 0 && !found);
+
+        if(found){
+            cout << "\nYou found the word '" << word << "'!" << endl;
+        } else {
+            cout << "\nYou FAILED to guess the word '" << word << "'!" << endl;
+        }
+    }
+
+    return 0;
+}
+
+int hashKey(string word, int numItems){
+    int num = 0;
+    for(int i = 0; i < word.size(); i++){
+        num += word.at(i)*3;
+    }
+    num = num % numItems;
+    return num;
+}
+
+void display(int numGuesses, string word, bool letters[], bool isIn[], ostream &os){
+    char c;
+    //prints letters in word guessed so far
+    for(int i = 0; i < word.size(); i++){
+        if(isIn[i]){
+            os << word.at(i) << " ";
+        }else {
+            os << "_ ";
+        }
+    }
+
+    cout << "\nLetters Guessed: " << endl;
+    for(int i = 0; i < LETTERS_IN_ALPHABET; i++){
+        if(letters[i]){
+            c = 'a' + i;
+            os << c << " ";
+        }
+    }
+
+    cout << "\nGuesses Remaining: " << numGuesses << endl;
+}
